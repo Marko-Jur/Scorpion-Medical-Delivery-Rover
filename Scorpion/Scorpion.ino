67c99907873b41d4097675876a2e4a1f2762d99d
Marko-Jur <47679103+Marko-Jur@users.noreply.github.com>
2020-12-24 22:45:18 -0800

    Update Scorpion.ino

--- Scorpion/Scorpion.ino
+++ Scorpion/Scorpion.ino
@@ -1,7 +1,16 @@
 //Main controller for Scorpion
+#include "Pin_Assignments.h"
+#include "Scorpion_Libraries.h"
+#include "Grab.h"
+#include "Comms.h"
+
 
 void setup(){
 
     Serial.begin(9600);
 
+}
+
+void loop(){
+    
 }
