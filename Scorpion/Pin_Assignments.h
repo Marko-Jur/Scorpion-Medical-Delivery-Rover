69a8a99ba6983fc2a002c8fcdd2977fa43c5b41f
Marko-Jur <47679103+Marko-Jur@users.noreply.github.com>
2020-12-24 22:47:22 -0800

    Update Pin_Assignments.h

--- Scorpion/Pin_Assignments.h
+++ Scorpion/Pin_Assignments.h
@@ -3,4 +3,21 @@
 #ifndef _PINASSIGNMENTS_H    
 #define _PINASSIGNMENTS_H
 
+//Motors
+#define MOTOR_RIGHT_A
+#define MOTOR_RIGHT_B
+#define MOTOR_RIGHT_ENABLE
+
+#define MOTOR_LEFT_A
+#define MOTOR_LEFT_B
+#define MOTOR_ELEFT_ENABLE
+
+//Actuators
+#define LINEAR_ACTUATOR
+#define SERVO
+
+//Receiver
+#define RX 
+
+
 #endif
