453881b3bd974a5e36746e5a483333707e488fac
Marko-Jur <47679103+Marko-Jur@users.noreply.github.com>
2020-12-26 19:06:35 -0800

    Update Grab.cpp
    Added in Function descriptions

--- Scorpion/Grab.cpp
+++ Scorpion/Grab.cpp
@@ -5,10 +5,12 @@
 
 //Declare servo and linear actuator objects, you might need to use the Sevro.h library here
 
+//Add in Function Description Here
 void setupGrab(){
  //Setup the servo and Linear actuator pins 
 }
 
+//Add in Function Description Here
 void Grab(int transmitter_values[8]){
  //Make the servos move. Wait for Switch B on transmitter_values[5] to turn HIGH and then the servos should move
   
