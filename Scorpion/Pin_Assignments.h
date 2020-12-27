be206fa6307d4b5ee3d923fa5e94fbb442201767
Marko-Jur <47679103+Marko-Jur@users.noreply.github.com>
2020-12-26 21:13:55 -0800

    Added in Pin definitions

--- Scorpion/Pin_Assignments.h
+++ Scorpion/Pin_Assignments.h
@@ -4,20 +4,29 @@
 #define _PINASSIGNMENTS_H
 
 //Motors
-#define MOTOR_RIGHT_A
-#define MOTOR_RIGHT_B
-#define MOTOR_RIGHT_ENABLE
+#define MOTOR_RIGHT_A 7
+#define MOTOR_RIGHT_B  6
+#define MOTOR_RIGHT_ENABLE 11
 
-#define MOTOR_LEFT_A
-#define MOTOR_LEFT_B
-#define MOTOR_LEFT_ENABLE
+#define MOTOR_LEFT_A 5
+#define MOTOR_LEFT_B 12
+#define MOTOR_LEFT_ENABLE 3
 
 //Actuators
-#define LINEAR_ACTUATOR
-#define SERVO
+#define LINEAR_ACTUATOR 9
+#define SERVO 10
 
 //Receiver
-#define RX 
+#define RX 2
+
+//Lasers
+#define LASER_A 
+#define LASER_B
+
+//FPV
+
+#define FPV_A
+#define FPV_B
 
 
 #endif
