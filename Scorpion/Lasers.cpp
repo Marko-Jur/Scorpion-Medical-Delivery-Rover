a11a4a55692ea68e625aa6dd9be3cb0f9c822241
Marko-Jur <47679103+Marko-Jur@users.noreply.github.com>
2020-12-26 21:10:13 -0800

    Added in Functions

--- Scorpion/Lasers.cpp
+++ Scorpion/Lasers.cpp
@@ -2,3 +2,14 @@
 
 #include "Pin_Assignments.h"
 #include "Scorpion_Libraries.h"
+
+//Add function description here
+void setuplasers(){
+  
+}
+
+//Add function description here
+void laserControl(){
+  
+  
+}
