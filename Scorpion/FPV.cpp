a67cf7fa4e0962ec2767eee73ab9931039459fb6
Marko-Jur <47679103+Marko-Jur@users.noreply.github.com>
2020-12-26 21:11:37 -0800

    Added in Functions

--- Scorpion/FPV.cpp
+++ Scorpion/FPV.cpp
@@ -2,3 +2,15 @@
 
 #include "Pin_Assignments.h"
 #include "Scorpion_Libraries.h"
+
+//Add function description here
+void setupFpv(){
+  
+}
+
+//Add in function description here
+void fpvControl(int SWITCH_B){
+  
+  
+  
+}
