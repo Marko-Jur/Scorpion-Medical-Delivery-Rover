6bb9f8cec1ce4eeac025f0316b051675ddf91d1e
Marko-Jur <47679103+Marko-Jur@users.noreply.github.com>
2020-12-26 19:10:07 -0800

    Update Scorpion.ino

--- Scorpion/Scorpion.ino
+++ Scorpion/Scorpion.ino
@@ -3,14 +3,25 @@
 #include "Scorpion_Libraries.h"
 #include "Grab.h"
 #include "Comms.h"
+#include "Fpv.h"
+#include "Lasers.h"
 
-
+//Add in Function Description Here
 void setup(){
 
     Serial.begin(9600);
+    
+    //Setup all the components here. You should call about 3-5 functions here
+    
+    
 
 }
 
+//Add in Function Description Here
 void loop(){
+    
+    /* Call functions here to execute the functionality of Scorpion. Your job will be to pass in inputs and extract outputs to functions where necessary. You need to call
+    *  about 5-7 functions here
+    */
     
 }
