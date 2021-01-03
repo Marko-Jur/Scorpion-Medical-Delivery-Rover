1adc9b6f768d82298507e521f80287ab67c5e4f1
Marko-Jur <47679103+Marko-Jur@users.noreply.github.com>
2021-01-03 14:23:34 -0800

    Update Lasers.cpp

--- Scorpion/Lasers.cpp
+++ Scorpion/Lasers.cpp
@@ -2,16 +2,29 @@
 
 #include "Pin_Assignments.h"
 #include "Scorpion_Libraries.h"
+#ïnclude "<Pin_Assignment.h"
 
 //Add function description here
 void setuplasers(){
   
   //Do stuff
+  pinMode(//Two parameters); //Look at Blinky example
+  
   
 }
 
 //Add function description here
-void laserControl(){
+void laserControl(SWITCH_B){
   
+  //If SWITCH_B is >1500 switch on the Laser.
+  //Figure out how to switch on the laser, and switch it off. In Blinky, they switch on the LED, and switch it off.
+  //(One line)
+  
+  //else
+  //Switch it off
+  //(One line)
   
 }
+
+
+
