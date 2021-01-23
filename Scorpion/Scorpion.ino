8e48cb2154a6b57cd06447727f44c004647532c0
marklong-pg <76639358+marklong-pg@users.noreply.github.com>
2021-01-23 13:51:25 -0800

    Update Scorpion.ino

--- Scorpion/Scorpion.ino
+++ Scorpion/Scorpion.ino
@@ -1,10 +1,10 @@
 //Main controller for Scorpion
-#include "Pin_Assignments.h"
-#include "Scorpion_Libraries.h"
-#include "Grab.h"
-#include "Comms.h"
-#include "Fpv.h"
-#include "Lasers.h"
+#include <Pin_Assignments.h>
+#include <Scorpion_Libraries.h>
+#include <Grab.h>
+#include <Comms.h>
+#include <Fpv.h>
+#include <Lasers.h>
 
 int raw_rc_values[8] = {0,0,0,0,0,0,0,0}; // This is the array you need to pass into the readRc function.
 
