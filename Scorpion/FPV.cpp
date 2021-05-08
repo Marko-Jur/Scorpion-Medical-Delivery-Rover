85982686d7dec0c1ec5f88f87533652ceb569378
TayyibChohan <46332430+TayyibChohan@users.noreply.github.com>
2021-05-08 12:06:15 -0700

    Added ability to flip FPV camera

--- Scorpion/FPV.cpp
+++ Scorpion/FPV.cpp
@@ -2,15 +2,24 @@
 
 #include "Pin_Assignments.h"
 #include "Scorpion_Libraries.h"
+const int FRONT_POS = 0; // ready position
+const int BACK_POS = 180; //grab 
+Servo camServo;
 
 //Add function description here
 void setupFpv(){
-  
+  camServo.attach(CAM_SERVO);
 }
 
 //Add in function description here
 void fpvControl(int SWITCH_C){
+    
+  if (SWITCH_C > 1300){
+    camServo.write(FRONT_POS);
+  }
+  else{
+    camServo.write(FRONT_POS);
+  }
   
-  
-  
+}
 }
